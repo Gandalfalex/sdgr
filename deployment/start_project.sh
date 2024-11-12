fa59328c77e8f8b75071f799bf72d46ccf96859d
Felix Fischer <ich@Mac.fritz.box>
2024-11-12 19:39:12 +0100

    changed deployment strategy

---
+++ deployment/start_project.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+read -p "Please enter the environment (prod/dev): " ENVIRONMENT
+
+# Convert input to lowercase
+ENVIRONMENT=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]')
+
+# Validate the input
+if [[ "$ENVIRONMENT" != "prod" && "$ENVIRONMENT" != "dev" ]]; then
+  echo "Invalid environment. Please enter 'prod' or 'dev'."
+  exit 1
+fi
+
+
+SPRING_CONTAINER_NAME="spring-backend-$ENVIRONMENT"
+DJANGO_CONTAINER_NAME="django-$ENVIRONMENT"
+DB_CONTAINER_NAME="sus-db-$ENVIRONMENT"
+
+DB_USER="sample"
+DB_NAME="sample"
+DATA_SQL_PATH="../data/inital_data.sql"
+DATA_SCHEMA_PATH="../data/schema.sql"
+
+echo "Starting Docker Compose services..."
+
+cd $ENVIRONMENT
+
+docker network create sus-network
+
+docker compose up -d "db" --wait
+docker exec -i "$DB_CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+
+docker cp "$DATA_SCHEMA_PATH" "$DB_CONTAINER_NAME":/schema.sql
+docker exec -i "$DB_CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f /schema.sql
+
+
+echo "Loading data into the database from data.sql..."
+docker cp "$DATA_SQL_PATH" "$DB_CONTAINER_NAME":/data.sql
+docker exec -i "$DB_CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f /data.sql
+
+docker compose up -d --wait
+docker compose down
