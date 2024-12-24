888523c36d90828627f17d5cac40fddd769d085f
Gandalfalex <feragon98@gmx.de>
2024-12-24 12:19:54 +0100

    updated version

--- frontend/src/api/ApiConnector.tsx
+++ frontend/src/api/ApiConnector.tsx
@@ -1,39 +1,52 @@
 import axios from "axios";
 
+export const BASE_URL = "http://t123abcdefg.mooo.com:80";
+const refreshToken = sessionStorage.getItem("refreshToken");
 
-export const BASE_URL = 'http://t123abcdefg.mooo.com:80'
-const BASE_URL_SPRING =  'http://localhost:8080'; // '/springboot'; //
-const BASE_URL_DJANGO = 'http://localhost:8000'; // '/django'; //
-const refreshToken = sessionStorage.getItem('refreshToken');
-export const WS_DJANGO_URL =  'ws://localhost:8000';// 'ws://t123abcdefg.mooo.com:80/django'; //
-export const WS_SPRING_URL = 'ws://localhost:8080'; // 'ws://t123abcdefg.mooo.com:80/springboot';//
+const { protocol, hostname } = window.location;
+const apiProtocol = protocol === "https:" ? "https:" : "http:";
+const wsProtocol = protocol === "https:" ? "wss:" : "ws:";
+
+// Dynamically set base URLs for Spring and Django APIs
+export const BASE_URL_SPRING = `${apiProtocol}//${hostname}:3000/springboot`;
+export const BASE_URL_DJANGO = `${apiProtocol}//${hostname}:3000/django`;
+
+// Dynamically set WebSocket URLs
+export const WS_DJANGO_URL = `${wsProtocol}//${hostname}:3000/django`;
+export const WS_SPRING_URL = `${wsProtocol}//${hostname}:3000/springboot`;
 
 const api2 = axios.create({
-    baseURL: `${BASE_URL_SPRING}`,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: `${BASE_URL_SPRING}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-
 export function loginUser(email: string, password: string) {
-    console.log(email)
-    return api2.post(`/api/v1/auth/login`, {email, password}).then(res => res.data);
+  return api2
+    .post(`/api/v1/auth/login`, { email, password })
+    .then((res) => res.data);
 }
 
-export function signupUser(email: string, password: string, firstName: string, lastName: string) {
-    return api2.post(`/api/v1/auth/signup`, {email, password, firstName, lastName}).then(res => res.data);
+export function signupUser(
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+) {
+  return api2
+    .post(`/api/v1/auth/signup`, { email, password, firstName, lastName })
+    .then((res) => res.data);
 }
-
 
 function refreshTokenRequest() {
-    return api2.post(`/api/v1/auth/refresh`, {token: refreshToken})
-        .then(response => {
-            sessionStorage.setItem('jwt', response.data.token);
-            return response.data.token;
-        });
+  return api2
+    .post(`/api/v1/auth/refresh`, { token: refreshToken })
+    .then((response) => {
+      sessionStorage.setItem("jwt", response.data.token);
+      return response.data.token;
+    });
 }
-
 
 /**
  * Create an instance of the API using the Axios library.
@@ -43,42 +56,43 @@ function refreshTokenRequest() {
  * @returns {object} - An instance of the API.
  */
 export function createAPI(baseURL: string, path?: string) {
-    let url = path ? baseURL + path : baseURL;
+  let url = path ? baseURL + path : baseURL;
 
-    const api = axios.create({
-        baseURL: url,
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+  const api = axios.create({
+    baseURL: url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    api.interceptors.request.use((config) => {
-        const token = sessionStorage.getItem('jwt')
-        config.headers.Authorization = token ? `Bearer ${token}` : '';
-        return config;
-    });
+  api.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem("jwt");
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+  });
 
-    api.interceptors.response.use(
-        response => response,
-        async error => {
-            const originalRequest = error.config;
+  api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
 
-            if (error.response.status === 403 && !originalRequest._retry) {
-                originalRequest._retry = true;
-                const newAccessToken = await refreshTokenRequest();
-                axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-                originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                return axios(originalRequest);
-            } else if (error.response.status === 404) {
-                console.log(error.response.data);
-            } else if (error.response.status === 400) {
-                console.log(error.response.data);
-            }
+      if (error.response.status === 403 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const newAccessToken = await refreshTokenRequest();
+        axios.defaults.headers.common["Authorization"] =
+          `Bearer ${newAccessToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        return axios(originalRequest);
+      } else if (error.response.status === 404) {
+        console.log(error.response.data);
+      } else if (error.response.status === 400) {
+        console.log(error.response.data);
+      }
 
-            return Promise.reject(error);
-        }
-    );
-    return api;
+      return Promise.reject(error);
+    },
+  );
+  return api;
 }
 
 export const springAPI = createAPI(BASE_URL_SPRING);
